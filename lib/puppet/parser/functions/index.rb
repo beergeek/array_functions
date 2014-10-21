0702667915ac13cebccf7a1ba0efd8210286c6bf
beergeek <brett.gray@puppetlabs.com>
2014-10-22 01:40:54 +1100

    initial commit

---
+++ lib/puppet/parser/functions/index.rb
@@ -0,0 +1,11 @@
+module Puppet::Parser::Functions
+  newfunction('index', :type => :rvalue, :arity => 2) do |args|
+    raise(Puppet::ParserError, "This function requires an array") if args[0] != Array
+
+    if  args[0](args[1]) 
+      return true
+    else
+      return false
+    end
+  end
+end
