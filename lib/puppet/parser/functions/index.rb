e3bd8b2a871ce7aeb4fbbe49f53098d19622eaa4
beergeek <brett.gray@puppetlabs.com>
2014-10-29 05:18:37 +1100

    make it less shit

--- lib/puppet/parser/functions/index.rb
+++ lib/puppet/parser/functions/index.rb
@@ -2,7 +2,7 @@ module Puppet::Parser::Functions
   newfunction('index', :type => :rvalue, :arity => 2) do |args|
     raise(Puppet::ParserError, "This function requires an array") if args[0] != Array
 
-    if  args[0](args[1]) 
+    if  args[0].member?(args[1])
       return true
     else
       return false
